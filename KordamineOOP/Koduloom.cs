37f29a2b839e55df7adae77f2395c29d083446f5
ArtemStryzhakov <77277962+ArtemStryzhakov@users.noreply.github.com>
2022-09-07 09:54:49 +0300

    Add files via upload

--- KordamineOOP/Koduloom.cs
+++ KordamineOOP/Koduloom.cs
@@ -1,3 +1,47 @@
+<<<<<<< HEAD:Koduloomad/KordamineOOP/Koduloom.cs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KordamineOOP
+{
+    abstract class Koduloom
+    {
+        public string nimi;
+        public string varv;
+        //public string sugu;
+
+        public enum sugu { isane, emane};
+        public sugu loomaSugu;
+        public double kaal;
+        public int vanus;
+        public string nahaalsus;
+
+        public Koduloom(string nimi, string nahaalsus)
+        {
+            this.nimi = nimi;
+            this.varv = nahaalsus;
+        }
+
+        public Koduloom(string nimi, string varv, sugu loomaSugu, double kaal = 0.0, int vanus = 0, string nahaalsus = "")
+        {
+            this.nimi = nimi;
+            this.varv = varv;
+            this.loomaSugu = loomaSugu;
+            this.kaal = kaal;
+            this.vanus = vanus;
+            this.nahaalsus = nahaalsus;
+        }
+
+        public Koduloom() { }
+        public abstract string print_Info();
+        public abstract void print_Haal();
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,3 +77,4 @@ namespace KordamineOOP
         public abstract void print_Haal();
     }
 }
+>>>>>>> 9ac31e0a1a7724520760ca2ad1dd272f693df1c5:KordamineOOP/Koduloom.cs
--- KordamineOOP/Program.cs
+++ KordamineOOP/Program.cs
@@ -14,13 +14,37 @@ veelkoer.muudaVanus(3);
 veelkoer.print_Info();
 Console.WriteLine("----------------------------------------------------------------------------");
 //Raccoon
-Raccoon raccoon = new Raccoon(Raccoon.tougR.Eastern_raccoon, "Maksim", "black", Koduloom.sugu.isane, 10, 4, "8/10");
-raccoon.print_Info();
-raccoon.print_Haal();
-Console.WriteLine(raccoon.print_Info());
+Raccoon raccoonMaksim = new Raccoon(Raccoon.tougR.Eastern_raccoon, "Maksim", "black", Koduloom.sugu.isane, 10, 4, "8/10");
+Raccoon raccoonLjoha = new Raccoon(Raccoon.tougR.Florida_raccoon, "Ljoha", "gray", Koduloom.sugu.isane, 15, 5, "10/10");
+Raccoon raccoonAndrei = new Raccoon(Raccoon.tougR.Texas_raccoon, "Andrei", "black", Koduloom.sugu.isane, 13, 3, "5/10");
+raccoonMaksim.print_Info();
+raccoonMaksim.print_Haal();
+Console.WriteLine(raccoonMaksim.print_Info());
+
+List<Koduloom> raccoonList = new List<Koduloom>();
+raccoonList.Add(raccoonMaksim);
+raccoonList.Add(raccoonLjoha);
+raccoonList.Add(raccoonAndrei);
 
 using (StreamWriter writer = new StreamWriter("C:\\Users\\opilane\\source\\repos\\Artem Stryzhakov TARpv20\\raccoon.txt"))
 {
-    writer.WriteLine(raccoon.print_Info());
+	foreach (Koduloom x in raccoonList)
+	{
+        writer.WriteLine(x.nimi + "," + x.nahaalsus + ",");
+    }
 }
-string readText = File.ReadAllText("C:\\Users\\opilane\\source\\repos\\Artem Stryzhakov TARpv20\\raccoon.txt");
+Console.WriteLine("\n");
+List<Raccoon> tootajad = new List<Raccoon>();
+StreamReader sr = new StreamReader("C:\\Users\\opilane\\source\\repos\\Artem Stryzhakov TARpv20\\raccoon.txt");
+string text;
+while ((text = sr.ReadLine()) != null)
+{
+    string[] rida = text.Split(',');
+    tootajad.Add(new Raccoon(rida[0], rida[1]));
+}
+sr.Close();
+foreach (var item in tootajad)
+{
+    Console.WriteLine($"Nimi: {item.nimi}, Nahaalsus: {item.nahaalsus}");
+}
+Console.ReadLine();
